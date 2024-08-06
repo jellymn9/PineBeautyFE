4f41a028717a40803b6a632d7b3f8f3a9ae9fece
Jelena <jelena.mandzic@gmail.com>
2024-08-06 12:30:11 +0200

    create routing draft

--- src/main.tsx
+++ src/main.tsx
@@ -2,27 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Home from "./pages/Home/Home.tsx";
+import routes from "./routes";
 import "./index.css";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Home />,
-    //loader: rootLoader,
-    // children: [
-    //   {
-    //     path: "team",
-    //     element: <Team />,
-    //     loader: teamLoader,
-    //   },
-    // ],
-  },
-]);
+const router = createBrowserRouter(routes);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    {/* <App /> */}
   </React.StrictMode>
 );
