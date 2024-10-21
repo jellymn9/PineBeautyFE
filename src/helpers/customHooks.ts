f4705aaa6d262f4b2317e77573f85ebc6e73d6cb
Jelena <jelena.mandzic@gmail.com>
2024-10-21 15:11:43 +0200

    Move logic to custom hook

--- src/helpers/customHooks.ts
+++ src/helpers/customHooks.ts
@@ -1,10 +1,13 @@
-import { useEffect, useState } from "react";
+import { RefObject, useEffect, useState } from "react";
 
 function useScrollLocation(elementHeightFromBottom: number) {
   const [isPointReached, setIsPointReached] = useState(false);
 
   useEffect(() => {
     function handleScroll() {
+      //const t = document.getElementsByTagName("section")[1].scrollHeight;
+      // console.log(document.getElementsByTagName("section")[1].scrollHeight);
+
       const pointForTrigger = window.innerHeight - elementHeightFromBottom; // use height other than innerHeight!
       const isTrigger = window.scrollY >= pointForTrigger;
       if (isTrigger !== isPointReached) {
@@ -21,4 +24,38 @@ function useScrollLocation(elementHeightFromBottom: number) {
   return isPointReached;
 }
 
-export { useScrollLocation };
+function useElementHeight(element: RefObject<HTMLElement>) {
+  const [bla, setBla] = useState(false);
+
+  useEffect(() => {
+    const current = element.current;
+    function handleScroll() {
+      console.log(1);
+      if (current !== null) {
+        const elementScrollableHeight = current.scrollHeight - 236;
+        const elementScrolledFromTop = current.scrollTop;
+        console.log(
+          "tf ",
+          elementScrollableHeight - elementScrolledFromTop <= 70
+        );
+        if (elementScrollableHeight - elementScrolledFromTop <= 70 && !bla) {
+          console.log(2);
+          //dispatch(fetchProductsThunk());
+          setBla(true);
+        } else {
+          setBla(false);
+        }
+      }
+    }
+
+    current !== null && current.addEventListener("scroll", handleScroll);
+
+    return () => {
+      current !== null && current.removeEventListener("scroll", handleScroll);
+    };
+  });
+
+  return bla;
+}
+
+export { useScrollLocation, useElementHeight };
--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 
 import { useAppSelector, useAppDispatch } from "../../withTypes";
 import { fetchProductsThunk } from "../../state/reducers/productReducer";
 import { productsSelector } from "../../state/selectors";
-import { useScrollLocation } from "../../helpers/customHooks";
+import { useElementHeight, useScrollLocation } from "../../helpers/customHooks";
 import { footerHeight } from "../../utils/constants";
 import ProductFilters from "../../components/ProductFilters/ProductFilters";
 import ProductsList from "../../components/ProductsList/ProductsList";
@@ -14,15 +14,21 @@ import {
   SectionHeading,
 } from "./ProductsStyled";
 
+const productsHeading = "All products";
+const productsDescription =
+  "Searching for a sustainable alternative to liquid shampoo? We ‘ve got you covered! Our solid shampoos and hair soaps are fully biodegradable, 100% natural, and they come with an eco-friendly packaging. We offer a wide range of shampoo bars and hair soaps for all hair types and special needs. Find the right one for you and keep your hair strands healthy and shiny!";
+
 function Products() {
   const dispatch = useAppDispatch();
   const productSectionRef = useRef<HTMLElement>(null);
 
-  const [bla, setBla] = useState(false);
+  //const [bla, setBla] = useState(false);
 
   const { products, status } = useAppSelector(productsSelector);
 
   const triggerDataFetch = useScrollLocation(footerHeight);
+
+  const isBottomReached = useElementHeight(productSectionRef);
 
   useEffect(() => {
     dispatch(fetchProductsThunk());
@@ -30,39 +36,35 @@ function Products() {
   }, []);
 
   useEffect(() => {
-    if (triggerDataFetch) {
+    if (isBottomReached) {
       dispatch(fetchProductsThunk());
     }
-  }, [triggerDataFetch, dispatch]);
+  }, [isBottomReached, dispatch]);
 
-  const handleScroll = () => {
-    console.log(1);
-    if (productSectionRef.current !== null) {
-      const elementScrollableHeight =
-        productSectionRef.current.scrollHeight - 236;
-      const elementScrolledFromTop = productSectionRef.current.scrollTop;
-      console.log(
-        "tf ",
-        elementScrollableHeight - elementScrolledFromTop <= 70
-      );
-      if (elementScrollableHeight - elementScrolledFromTop <= 70 && !bla) {
-        console.log(2);
-        dispatch(fetchProductsThunk());
-        setBla(true);
-      } else {
-        setBla(false);
-      }
-    }
-  };
-
-  const productsHeading = "All products";
-  const productsDescription =
-    "Searching for a sustainable alternative to liquid shampoo? We ‘ve got you covered! Our solid shampoos and hair soaps are fully biodegradable, 100% natural, and they come with an eco-friendly packaging. We offer a wide range of shampoo bars and hair soaps for all hair types and special needs. Find the right one for you and keep your hair strands healthy and shiny!";
+  // const handleScroll = () => {
+  //   console.log(1);
+  //   if (productSectionRef.current !== null) {
+  //     const elementScrollableHeight =
+  //       productSectionRef.current.scrollHeight - 236;
+  //     const elementScrolledFromTop = productSectionRef.current.scrollTop;
+  //     console.log(
+  //       "tf ",
+  //       elementScrollableHeight - elementScrolledFromTop <= 70
+  //     );
+  //     if (elementScrollableHeight - elementScrolledFromTop <= 70 && !bla) {
+  //       console.log(2);
+  //       dispatch(fetchProductsThunk());
+  //       setBla(true);
+  //     } else {
+  //       setBla(false);
+  //     }
+  //   }
+  // };
 
   return (
     <Container>
       <ProductFilters />
-      <ProductsSection onScroll={handleScroll} ref={productSectionRef}>
+      <ProductsSection ref={productSectionRef}>
         {products.list.length ? (
           <>
             <SectionHeading>{productsHeading}</SectionHeading>
