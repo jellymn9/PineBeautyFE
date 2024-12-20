1e67bee44bf7cd211062599ef8fe8eb5a44e5f06
Jelena <jelena.mandzic@gmail.com>
2024-12-20 20:26:29 +0100

    Add netlify.toml file

--- src/APIs/endpoints.ts
+++ src/APIs/endpoints.ts
@@ -1,6 +1,6 @@
 const baseURL = import.meta.env.API_BASE_URL || "http://localhost:3000";
 
-console.log(import.meta.env.API_BASE_URL);
+console.log("bla: ", import.meta.env.API_BASE_URL);
 
 export default {
   products: baseURL + "/products",
