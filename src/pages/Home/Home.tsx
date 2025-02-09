2c0c1ce01ee9b1ad70905bc26a98e12b6dff8b68
Jelena <jelena.mandzic@gmail.com>
2025-02-09 18:37:43 +0100

    Add Hero to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,15 @@
-import "./Home.css";
+import Button from "../../components/Button/Button";
+import { FlagHeading, Hero, MainHeading } from "./HomeStyled";
 
 function Home() {
   console.log("test");
   return (
     <div>
+      <Hero>
+        <FlagHeading>low waste skincare</FlagHeading>
+        <MainHeading>Clean beauty with a story</MainHeading>
+        <Button text="shop" />
+      </Hero>
       <p>This is Home page</p>
     </div>
   );
---
+++ src/pages/Home/HomeStyled.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import colors from "../../utils/colors";
+
+export const Hero = styled.div`
+  background-image: url("gen3.jpg");
+  padding: 60px 40px;
+`;
+
+export const FlagHeading = styled.p`
+  text-transform: uppercase;
+`;
+
+export const MainHeading = styled.p`
+  font-size: 38px;
+`;
+
+export const SectionParagraph = styled.p``;
+
+export const SustainableAndPure = styled.div`
+  background-color: ${colors.timberwolf};
+`;
