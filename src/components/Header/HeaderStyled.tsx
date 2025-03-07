369741d36e311663de7936d456c33b23917168f2
Jelena <jelena.mandzic@gmail.com>
2025-03-07 18:34:51 +0100

    Add bar animation to non-icon links in navbar

--- src/components/Header/HeaderStyled.tsx
+++ src/components/Header/HeaderStyled.tsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 import colors from "../../utils/colors";
 
+const linkStyledPadding = 16;
+
 export const ContainerBlock = styled.div`
   height: 135px;
 `;
@@ -43,25 +45,52 @@ export const InnerContainer = styled.div`
   display: flex;
   justify-content: space-between;
   width: 100%;
-`;
-
-export const LinksContainerNav = styled.nav`
-  display: flex;
-  grid-gap: 36px;
 
   align-items: center;
 `;
 
+export const LinksContainerNav = styled.nav`
+  display: flex;
+  align-items: center;
+  background-color: transparent;
+`;
+
 export const LinkStyled = styled(Link)`
+  height: 100%;
+  display: inline-block;
+
   text-decoration: none;
   color: ${colors.black};
   text-transform: uppercase;
+  padding: 0 ${linkStyledPadding}px;
 
   font-family: Montserrat-Variable;
   letter-spacing: 0.02em;
   font-weight: 500;
-  font-size: 16px;
-  line-height: 26px;
+  font-size: 14px;
+  line-height: 24px;
+  background-color: transparent;
+
+  //border: 1px solid red;
+`;
+
+export const HoverBar = styled.div<{
+  $linkWidth: number;
+  $step: number;
+}>`
+  height: 2px;
+  top: 80px;
+  position: absolute;
+  z-index: -1;
+
+  transition: width 0.4s cubic-bezier(0.215, 0.61, 0.355, 1),
+    transform 0.3s linear;
+
+  width: 0;
+  background-color: ${colors.blackTransparent1};
+
+  transform: ${({ $step }) => ` translate(${$step + linkStyledPadding}px)`};
+  width: ${({ $linkWidth }) => ` ${$linkWidth - 2 * linkStyledPadding}px`};
 `;
 
 export const MobileContainer = styled.div`
@@ -69,4 +98,23 @@ export const MobileContainer = styled.div`
   justify-content: space-between;
   padding: 26px 16px;
   align-items: center;
+`;
+
+export const CircleAnimation = styled.div`
+  a::before {
+    z-index: -1;
+    position: absolute;
+    display: none;
+    content: "";
+    height: 22px;
+    width: 22px;
+    border-radius: 16px;
+    background-color: ${colors.blackTransparent1};
+  }
+
+  &:hover {
+    a::before {
+      display: block;
+    }
+  }
 `;
