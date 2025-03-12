4280c968ec21b7ae5c4697b9cefcf3349fb680f6
Jelena <jelena.mandzic@gmail.com>
2025-03-12 18:28:49 +0100

    Replace style-based event triggering with target check in mouseover handler
    Add circle animation for icon-like links

--- src/components/Header/HeaderStyled.tsx
+++ src/components/Header/HeaderStyled.tsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 
 import colors from "../../utils/colors";
 
-const linkStyledPadding = 16;
-
 export const ContainerBlock = styled.div`
   height: 135px;
 `;
@@ -26,7 +24,6 @@ export const Container = styled.header<{
     $isSticky &&
     `
         background-color: ${colors.whiteTransparent1};
-        
         display: none;
     `}
 
@@ -53,16 +50,19 @@ export const LinksContainerNav = styled.nav`
   display: flex;
   align-items: center;
   background-color: transparent;
+  padding: 16px;
+  grid-gap: 20px;
+`;
+
+export const BarAnimationContainer = styled.div`
+  display: flex;
+  grid-gap: inherit;
 `;
 
 export const LinkStyled = styled(Link)`
-  height: 100%;
-  display: inline-block;
-
   text-decoration: none;
   color: ${colors.black};
   text-transform: uppercase;
-  padding: 0 ${linkStyledPadding}px;
 
   font-family: Montserrat-Variable;
   letter-spacing: 0.02em;
@@ -70,8 +70,6 @@ export const LinkStyled = styled(Link)`
   font-size: 14px;
   line-height: 24px;
   background-color: transparent;
-
-  //border: 1px solid red;
 `;
 
 export const HoverBar = styled.div<{
@@ -87,10 +85,10 @@ export const HoverBar = styled.div<{
     transform 0.3s linear;
 
   width: 0;
-  background-color: ${colors.blackTransparent1};
+  background-color: ${colors.blackTransparent2};
 
-  transform: ${({ $step }) => ` translate(${$step + linkStyledPadding}px)`};
-  width: ${({ $linkWidth }) => ` ${$linkWidth - 2 * linkStyledPadding}px`};
+  transform: ${({ $step }) => ` translate(${$step}px)`};
+  width: ${({ $linkWidth }) => ` ${$linkWidth}px`};
 `;
 
 export const MobileContainer = styled.div`
@@ -101,20 +99,34 @@ export const MobileContainer = styled.div`
 `;
 
 export const CircleAnimation = styled.div`
-  a::before {
-    z-index: -1;
-    position: absolute;
-    display: none;
-    content: "";
-    height: 22px;
-    width: 22px;
-    border-radius: 16px;
-    background-color: ${colors.blackTransparent1};
+  display: inherit;
+  grid-gap: inherit;
+
+  a {
+    position: relative;
   }
 
-  &:hover {
-    a::before {
-      display: block;
-    }
+  a::before {
+    content: "";
+
+    z-index: -1;
+    position: absolute;
+    top: 11px;
+    left: 11px;
+
+    height: 1px;
+    width: 1px;
+    opacity: 0;
+
+    border-radius: 50%;
+    background-color: ${colors.blackTransparent2};
+    outline: 0px solid ${colors.blackTransparent2};
+
+    transition: outline 0.3s, opacity 0.3s;
+  }
+
+  a:hover::before {
+    outline: 22px solid ${colors.blackTransparent2};
+    opacity: 1;
   }
 `;
