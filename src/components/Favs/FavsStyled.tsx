c2683cea27cd33408e5da84eb422534a43f7bce4
Jelena <jelena.mandzic@gmail.com>
2025-03-25 12:14:12 +0100

    Adapt desktop Favs component style

--- src/components/Favs/FavsStyled.tsx
+++ src/components/Favs/FavsStyled.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import breakpoints from "../../utils/breakpoints";
 
 export const SliderMainContainer = styled.div`
   display: flex;
@@ -7,7 +8,11 @@ export const SliderMainContainer = styled.div`
 `;
 
 export const SliderContainer = styled.div`
-  --card-container-width: 22.427vw;
+  --card-container-width: 48.427vw;
+
+  @media screen and (min-width: ${breakpoints.tablet}) {
+    --card-container-width: 22.427vw;
+  }
 
   width: calc(var(--card-container-width) * 3 + 2 * 16px) !important;
   //border: 1px solid red;
