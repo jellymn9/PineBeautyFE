ef2cae082af494f9f314b8db53425a79855b584b
Jelena <jelena.mandzic@gmail.com>
2025-03-27 10:59:52 +0100

    Update .env and image resource fetching

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -13,8 +13,9 @@ interface ProductCardPropsI {
   product: RawProductT;
 }
 
-const imageURL =
-  import.meta.env.VITE_R2_DEV_BUCKET_URL + "/oilBottleCustomFormat.jpg";
+const imageURL = import.meta.env.VITE_R2_PROD_BUCKET_URL
+  ? import.meta.env.VITE_R2_PROD_BUCKET_URL + "/oilBottleCustomFormat.jpg"
+  : import.meta.env.VITE_R2_DEV_BUCKET_URL + "/oilBottleCustomFormat.jpg";
 
 // const addToCart = () => {
 //   return; // come back to this
