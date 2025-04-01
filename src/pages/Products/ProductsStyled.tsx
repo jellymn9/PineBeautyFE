455bd59adcdc63fbf54625ed4c92db6818c8fcad
Jelena <jelena.mandzic@gmail.com>
2025-04-01 17:22:58 +0200

    Change products' banner text

--- src/pages/Products/ProductsStyled.tsx
+++ src/pages/Products/ProductsStyled.tsx
@@ -37,8 +37,9 @@ export const ProductsSection = styled.section`
 export const SectionHeading = styled.h1`
   padding-left: 10%;
   color: ${colors.white};
-  font-family: Cinzel-Bold;
+  font-family: Montserrat-Variable;
   font-weight: 500;
+  text-transform: uppercase;
 `;
 
 export const EmptyMessage = styled.p`
