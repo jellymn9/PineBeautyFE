7e382007afd19f586144b7714c2ad8b519965d0c
Jelena <jelena.mandzic@gmail.com>
2025-10-19 19:34:26 +0200

    Create ls cart update

--- src/helpers/cartHelper.ts
+++ src/helpers/cartHelper.ts
@@ -9,7 +9,32 @@ export const calculateSubtotal = (
 };
 
 export const setOrUpdateCartLS = (cartData: CartData) => {
-  window.localStorage.setItem("cart", JSON.stringify(cartData));
+  try {
+    const cartItem = window.localStorage.getItem("cart");
+    if (!cartItem) {
+      window.localStorage.setItem("cart", JSON.stringify(cartData));
+      return true;
+    }
+    const cartExisting = JSON.parse(cartItem);
+    const cartExistingItems = cartExisting.items ?? { items: {} };
+
+    Object.keys(cartData.items).forEach((productId) => {
+      const newItem = cartData.items[productId];
+      if (cartExistingItems[productId]) {
+        cartExistingItems[productId] = {
+          ...cartExistingItems[productId],
+          quantity: cartExistingItems[productId].quantity + 1,
+        };
+      } else {
+        cartExistingItems[productId] = newItem;
+      }
+    });
+
+    window.localStorage.setItem("cart", JSON.stringify(cartExisting));
+    return true;
+  } catch (e) {
+    console.error("Error setting or updating cart in localStorage:", e);
+  }
 };
 //improve add to cart new features
 //items: {
