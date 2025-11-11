7dfa5292195043273d4ebc13ef7f482d1a773f7a
Jelena <jelena.mandzic@gmail.com>
2025-11-11 14:49:21 +0100

    Comment dataMApper from cart old logic

--- src/helpers/dataMapper.ts
+++ src/helpers/dataMapper.ts
@@ -1,22 +1,22 @@
-import { CartItemI } from "@/utils/types/cartTypes";
-import { ProductI } from "@/utils/types/productTypes";
+// import { CartItemI } from "@/utils/types/cartTypes";
+// import { ProductI } from "@/utils/types/productTypes";
 
-export const mapQuantityToProducts = (
-  products: Array<ProductI>,
-  productsWithQuantity: Array<CartItemI>
-) => {
-  const productsMap = new Map(products.map((p) => [p.id, p]));
+// export const mapQuantityToProducts = (
+//   products: Array<ProductI>,
+//   productsWithQuantity: Array<CartItemI>
+// ) => {
+//   const productsMap = new Map(products.map((p) => [p.id, p]));
 
-  return productsWithQuantity.map((p) => {
-    const product = productsMap.get(p.id);
-    if (!product) {
-      // type guard
-      throw new Error(`Product with ID ${p.id} not found.`);
-    }
-    return {
-      ...product,
-      quantity: p.quantity,
-      //accPrice: product.price * p.quantity,
-    };
-  });
-};
+//   return productsWithQuantity.map((p) => {
+//     const product = productsMap.get(p.id);
+//     if (!product) {
+//       // type guard
+//       throw new Error(`Product with ID ${p.id} not found.`);
+//     }
+//     return {
+//       ...product,
+//       quantity: p.quantity,
+//       //accPrice: product.price * p.quantity,
+//     };
+//   });
+// };
