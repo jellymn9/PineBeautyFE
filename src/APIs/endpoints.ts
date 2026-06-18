e822118078a7634716c020d334329ea8220a8e83
Jelena <jelena.mandzic@gmail.com>
2026-06-18 17:49:29 +0200

    Remove unused file

--- src/APIs/endpoints.ts
+++
@@ -1,8 +0,0 @@
-//const baseURL = import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
-
-//console.log("bla: ", import.meta.env.VITE_API_BASE_URL);
-
-export default {
-  products: "/products",
-  auth: "/auth",
-};
